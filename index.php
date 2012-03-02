2c729b5817abce762291abf8f3e611a95cc8ecf3
Sargo Darya <info@sargodarya.de>
2012-03-02 19:57:04 +0100

    Added some stuff

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+function getFilesFromDir($dir) { 
+
+  $files = array(); 
+  if ($handle = opendir($dir)) { 
+    while (false !== ($file = readdir($handle))) { 
+        if ($file != "." && $file != "..") { 
+            if(is_dir($dir.'/'.$file)) { 
+                $dir2 = $dir.'/'.$file; 
+                $files[] = getFilesFromDir($dir2); 
+            } 
+            else { 
+              $files[] = $dir.'/'.$file; 
+            } 
+        } 
+    } 
+    closedir($handle); 
+  } 
+
+  return arrayFlat($files); 
+} 
+
+function arrayFlat($array) { 
+
+  foreach($array as $a) { 
+    if(is_array($a)) { 
+      $tmp = array_merge($tmp, arrayFlat($a)); 
+    } 
+    else { 
+      $tmp[] = $a; 
+    } 
+  } 
+
+  return $tmp; 
+} 
+
+function wrapInTags($array) {
+  $buffer = "";
+  foreach($array as $script) {
+    if(substr($script, -3) != '.js') continue;
+    $buffer .= "<script type=\"text/javascript\" src=\"$script\"></script>\r\n";
+  }
+  return $buffer;
+}
+
+$gameClasses = getFilesFromDir("js/Game");
+$threeXClasses = getFilesFromDir("js/THREE");
+
+$scriptArray = array_merge($gameClasses, $threeXClasses);
+$scripts = wrapInTags($scriptArray);
+
+$tmpl = file_get_contents("index.html");
+
+echo str_replace('<!--scripts-->', $scripts, $tmpl);
+
+?>
---
+++ js/Game/Map.js
@@ -0,0 +1,15 @@
+var Map	= function()
+{
+  this._isLoaded = false;
+};
+
+Map.prototype.loadFromServer = function(path) 
+{
+  // get map file here
+  // 
+};
+
+Map.prototype.parseMapData = function(mapData)
+{
+  
+};
